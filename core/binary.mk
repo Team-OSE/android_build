1d6084a766af352387e40f771eabfd3385902599
Chet Kener <Cl3Kener@gmail.com>
2015-01-18 07:15:52 -0500

    Introduce Modular TARGET_USE_PIPE
    * Allow for -pipe to be used. This flag actually has no effect on the generated code, but it makes the compilation process faster. It tells the compiler to use pipes instead of temporary files during the different stages of compilation, which uses more memory. On systems with low memory, GCC might get killed. In that case, do not use this flag.
    
    * From my personal experience, compilation time was ~5 minutes less using pipes on a "make -j8" build on my personal computer.  Times may improvement more when doing like a make -j16 or -j24 on a build server with lots of RAM. Also note that this may not speed things up on a SSD hard drive due to the faster write speeds, I only tested this on a regular hard drive.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/binary.mk

--- core/binary.mk
+++ core/binary.mk
@@ -101,6 +101,10 @@ ifeq ($(OSE_OPTIMIZE),true)
 include $(BUILD_SYSTEM)/graphite.mk
 endif
 
+ifeq ($(TARGET_USE_PIPE),true)
+include $(BUILD_SYSTEM)/pipe.mk
+endif
+
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.
