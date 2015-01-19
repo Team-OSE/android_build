316be09eead0695652f2752b0fdc1efbdc37ed7d
Doug Nottingham <Lownslow200sx@gmail.com>
2015-01-18 22:48:16 -0500

    Build: Use OSE Lollipop Sounds (2/2)

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -29,6 +29,8 @@ PRODUCT_PACKAGES := \
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dateformat=MM-dd-yyyy
+    ro.config.ringtone=Triton.ogg \
+    ro.config.notification_sound=Io.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -37,7 +39,7 @@ PRODUCT_LOCALES := en_US
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudioOSE.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
