26aa9ccd41e28af33c478036c2c04eae49f8558f
Chet Kener <Cl3Kener@gmail.com>
2015-02-01 22:15:54 -0500

    OSE Optimizations V2
    * Remove cc call options (they don't work in android build folder)
    * Added Strict
    * Fixed Graphite
    * Reorder flags
    * Added SaberMod Copyright because these ideas came from the SaberMod team.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

---
+++ core/strict.mk
@@ -0,0 +1,128 @@
+# Copyright (C) 2014 The SaberMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DISABLE_STRICT := \
+	camera.omap4 \
+	audio.primary.piranha \
+	libtee_client_api_driver \
+	mm-vdec-omx-test \
+	libOmxVenc \
+	libqsap_sdk \
+	lsof \
+	tinyxml \
+	camera.msm8084 \
+	hwcomposer.msm8084 \
+	bionic \
+	libavutil \
+	libavcodec \
+	gatt_testtool \
+	libwnndict \
+	libxml2 \
+	busybox \
+	libbusybox \
+	libfuse \
+	libmtdutils \
+	libstlport_static \
+	libdl \
+	libc_common \
+	stlport \
+	libcrypto_static \
+	libc_bionic \
+	libc_dns \
+	libc_tzcode \
+	libziparchive \
+	libtwrpmtp \
+	libfusetwrp \
+	libguitwrp \
+	busybox \
+	libuclibcrpc \
+	libziparchive-host \
+	libpdfiumcore \
+	libandroid_runtime \
+	libmedia \
+	libpdfiumcore \
+	libpdfium \
+	bluetooth.default \
+	logd \
+	mdnsd \
+	net_net_gyp \
+	libstagefright_webm \
+	libaudioflinger \
+	libmediaplayerservice \
+	libstagefright \
+	ping \
+	ping6 \
+	libdiskconfig \
+	libjavacore \
+	libfdlibm \
+	libvariablespeed \
+	librtp_jni \
+	libwilhelm \
+	libdownmix \
+	libldnhncr \
+	libqcomvisualizer \
+	libvisualizer \
+	libstlport \
+	libutils \
+	libandroidfw \
+	dnsmasq \
+	static_busybox \
+	content_content_renderer_gyp \
+	third_party_WebKit_Source_modules_modules_gyp \
+	third_party_WebKit_Source_platform_blink_platform_gyp \
+	third_party_WebKit_Source_core_webcore_remaining_gyp \
+	third_party_angle_src_translator_lib_gyp \
+	third_party_WebKit_Source_core_webcore_generated_gyp \
+	libc_gdtoa \
+	libc_openbsd \
+	libc \
+	libc_nomalloc \
+        libcurl \
+	libc_malloc \
+	libjemalloc
+
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_STRICT), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+else
+LOCAL_CONLYFLAGS := \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+else
+LOCAL_CPPFLAGS := \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+endif
+ifdef LOCAL_CLANG
+LOCAL_CONLYFLAGS += \
+	-Wstrict-aliasing=3
+LOCAL_CPPFLAGS += \
+	-Wstrict-aliasing=3
+else
+LOCAL_CONLYFLAGS += \
+	-Wstrict-aliasing=2
+LOCAL_CPPFLAGS += \
+	-Wstrict-aliasing=2
+endif
+endif
+#####
