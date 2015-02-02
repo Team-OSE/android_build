f07f1f8585e2477216649613bd663dec7629961f
Chet Kener <Cl3Kener@gmail.com>
2015-02-01 22:15:54 -0500

    OSE Optimizations V2
    * Remove cc call options (they don't work in android build folder)
    * Added Strict
    * Fixed Graphite
    * Reorder flags
    * Added SaberMod Copyright because these ideas came from the SaberMod team.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/binary.mk
+++ core/binary.mk
@@ -97,12 +97,37 @@ else
   endif
 endif
 
-ifeq ($(OSE_OPTIMIZE),true)
-include $(BUILD_SYSTEM)/graphite.mk
-endif
-
+# Copyright (C) 2014 The SaberMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Include custom gcc flags.  Seperate them so they can be easily managed.
 ifeq ($(TARGET_USE_PIPE),true)
 include $(BUILD_SYSTEM)/pipe.mk
+endif
+
+ifeq ($(OSE_OPTIMIZE),true)
+ifeq ($(STRICT_ALIASING),true)
+include $(BUILD_SYSTEM)/strict.mk
+endif
+
+ifeq ($(GRAPHITE_OPTS),true)
+ifndef LOCAL_IS_HOST_MODULE
+ifeq ($(LOCAL_CLANG),)
+include $(BUILD_SYSTEM)/graphite.mk
+endif
+endif
+endif
 endif
 
 # The following LOCAL_ variables will be modified in this file.
